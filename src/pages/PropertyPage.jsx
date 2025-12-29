932b14eccf97226921280cdf933bfc1ef0434117
Hussain Muffallal <hussainmufallal2004@gmail.com>
2025-12-29 22:53:23 +0530

    Add tabbed property details below image gallery

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css"
 
 
 function PropertyPage() {
@@ -69,6 +71,37 @@ function PropertyPage() {
                     />
                 ))}
             </div>
+
+            {/* Property Tabs */}
+            <Tabs style={{ marginTop: "20px" }}>
+                <TabList>
+                    <Tab>Description</Tab>
+                    <Tab>Floor Plan</Tab>
+                    <Tab>Map</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <p>{property.description}</p>
+                </TabPanel>
+
+                <TabPanel>
+                    <img
+                        src={`${import.meta.env.BASE_URL}${property.floorPlan}`}
+                        alt="Floor plan"
+                        style={{ width: "100%", maxWidth: "500px" }}
+                    />
+                </TabPanel>
+
+                <TabPanel>
+                    <iframe
+                        title="Property location"
+                        width="100%"
+                        height="300"
+                        loading="lazy"
+                        src={`https://www.google.com/maps?q=${property.coordinates.lat},${property.lng}&output=embed`}
+                    />
+                </TabPanel>
+            </Tabs>
 
             <p><strong>Price:</strong> ${property.price.toLocaleString()}</p>
             <p><strong>Location:</strong> {property.location}</p>
